8f4809b131f47781af91c00c3ebfebb9be8a8853
Vitor Gabriel Lehnen <107063776+vitorglehnen@users.noreply.github.com>
2023-08-19 22:16:24 -0300

    Updates

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import requests
 import smtplib
 
 data_ontem = date.today() - timedelta(days=1)
-data_hoje = date.today().strftime('%d-%m-%Y')
+data_hoje = date.today()
 
 
 def conexao_db():
@@ -50,7 +50,7 @@ def busca_valor_total():
     query.execute(f'''select sum(valor_total)
                     from (select i.quantidade * max(h.preco) as valor_total
     	            from item i 
-    	            inner join historicoprecos h on h.coditem = i.coditem and h.datacons = '{data_hoje}'
+    	            inner join historicoprecos h on h.coditem = i.coditem and h.datacons = CURRENT_DATE
     	            group by h.coditem,i.coditem) sub''')
 
     valor_total = query.fetchone()
@@ -64,19 +64,20 @@ def busca_media_precos():
     conexao = conexao_db()
     query = conexao.cursor()
 
-    query.execute(f'''SELECT i.descricao "Item",
+    query.execute(f'''
+        SELECT i.descricao "Item",
        CONCAT('R$', max(hp.preco)) "Preço hoje",
        CONCAT('R$', i.preco) "Preço comprado",
        COALESCE((CASE WHEN (i.preco > max(hp.preco)) THEN CONCAT('- ', ABS(ROUND(((max(hp.preco) - i.preco) / i.preco) * 100, 2)), '%')
        WHEN (i.preco < max(hp.preco)) THEN CONCAT('+ ', ABS(ROUND(((max(hp.preco) - i.preco) / i.preco) * 100, 2)), '%')
        END), '   0.00%') "Percentual",
-		i.quantidade "Qtde",
+	    i.quantidade "Qtde",
 		'R$' || (i.quantidade * max(hp.preco)) "Valor total"
        FROM historicoprecos hp
        INNER JOIN item i on i.coditem = hp.coditem
-       WHERE hp.datacons = '{data_hoje}' and hp.horacons = (SELECT max(hp1.horacons) 
+       WHERE hp.datacons = CURRENT_DATE and hp.horacons = (SELECT max(hp1.horacons) 
 															FROM historicoprecos hp1
-															WHERE hp1.datacons = '{data_hoje}' and
+															WHERE hp1.datacons = CURRENT_DATE and
 															hp1.coditem = hp.coditem)
        GROUP BY hp.coditem, i.coditem
        ORDER BY ROUND(((max(hp.preco) - i.preco) / i.preco) * 100, 2) DESC
@@ -130,8 +131,8 @@ for coditem, preco in lista_itens:
     conexao = conexao_db()
     query = conexao.cursor()
 
-    query.execute(f"INSERT INTO historicoprecos(controle, coditem, preco, datacons, horacons)"
-                  f"VALUES (nextval('gen_controle_histprecos'), {coditem}, {preco}, '{data_agora}', '{hora_agora}')")
+    query.execute(f"INSERT INTO historicoprecos(controle, coditem, preco)"
+                  f"VALUES (nextval('gen_controle_histprecos'), {coditem}, {preco})")
 
     conexao.commit()
     query.close()
